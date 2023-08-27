5efce827f7b3ac8e37461812cd2ca4a8f3d708fb
Harold Gaviria Londoño <hagaviria@hotmail.com>
2023-08-27 13:08:49 -0500

    card-update 1

--- abode/src/components/Card/card.jsx
+++ abode/src/components/Card/card.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import "./styles.css";
 import hotel from "../../assets/img/hotel1.jpg";
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-export default function Cards() {
+const  Cards=() =>{
+  const CardMediastyle={  height: "300px",width: "300px", borderRadius:"0.5rem",objectFi:"cover"}
+  const CardStyle={ justifyContent: "center",padding:" 1rem 3rem", }
+  
   return (
-    <div  className='cards-flex' >
-        <img src={hotel} className='card-img'/>
-        <div className='card-info'>
-            <h3 className='card-title'>Place</h3>
-            <div className='card-rating'>
-              <StarBorderRoundedIcon/>
-              <p>4.88</p>
-              
-        </div>
-
-        </div>
-      <p style={{marginTop:0}}>Beach and sea views</p>
-      
-    </div>
+    <Card style={CardStyle}>
+    <CardMedia
+      image={hotel}
+      title="Card image"
+      component="img"
+  style={CardMediastyle}
+ 
+    
+    />
+    <CardContent>
+      <Typography variant="h5"
+      sx={{display: "flex",justifyContent: "space-between",alignitems: "flex-start"}}
+       gutterBottom>
+        Place
+      </Typography>
+      <Typography variant="body2" color="textSecondary"
+      sx={{   display: "flex",
+        gap: "0.25rem",
+        alignItems: "center"}}>
+        <StarBorderRoundedIcon/>
+        4.88
+      </Typography>
+      <p style={{marginTop:0,display: "flex",
+    justifyCcontent: "space-between",
+    alignItems: "flex-start"}} >Beach and sea views</p>
+    </CardContent>
+    </Card>
   )
 }
+export default Cards
