7ea708cdeae320dd100e70f53d750669e38a7d4e
Harold Gaviria Londoño <hagaviria@hotmail.com>
2023-08-27 15:36:56 -0500

    update cards and aicon-filter

--- abode/src/components/Card/card.jsx
+++ abode/src/components/Card/card.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
 import hotel from "../../assets/img/hotel1.jpg";
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography, Grid } from "@mui/material";
+import CardData from './cardData';
+
 
 const  Cards=() =>{
   const CardMediastyle={  height: "300px",width: "300px", borderRadius:"0.5rem",objectFi:"cover"}
-  const CardStyle={ justifyContent: "center",padding:" 1rem 3rem", }
+  const CardStyle={ justifyContent: "center",padding:" 1rem 3rem", display:"flex",gap:"1rem"}
   
+ const cardData=CardData();
   return (
-    <Card style={CardStyle}>
-    <CardMedia
-      image={hotel}
-      title="Card image"
-      component="img"
-  style={CardMediastyle}
- 
-    
-    />
-    <CardContent>
-      <Typography variant="h5"
-      sx={{display: "flex",justifyContent: "space-between",alignitems: "flex-start"}}
-       gutterBottom>
-        Place
-      </Typography>
-      <Typography variant="body2" color="textSecondary"
-      sx={{   display: "flex",
-        gap: "0.25rem",
-        alignItems: "center"}}>
-        <StarBorderRoundedIcon/>
-        4.88
-      </Typography>
-      <p style={{marginTop:0,display: "flex",
-    justifyCcontent: "space-between",
-    alignItems: "flex-start"}} >Beach and sea views</p>
-    </CardContent>
-    </Card>
+    <div >
+    <Grid container spacing={2} style={CardStyle}
+    >
+      {cardData.map((item) => (
+        <Card key={item.title} >
+          <CardMedia
+            image={item.image}
+            title="Card image"
+            component="img"
+            style={CardMediastyle}
+          />
+          <CardContent>
+            <Typography variant="h5"
+              sx={{display: "flex", justifyContent: "space-between", alignitems: "flex-start"}}
+              gutterBottom>
+            {item.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary"
+            sx={{
+              display: "flex",
+              gap: "0.25rem",
+              alignItems: "center",
+            }}>
+            <StarBorderRoundedIcon />
+            {item.rating}
+          </Typography>
+          <p style={{ marginTop: 0, display: "flex", justifyContent: "space-between", alignItems: "flex-start" }}>
+            Beach and sea views
+          </p>
+        </CardContent>
+      </Card>
+    ))}
+    </Grid>
+  </div>
   )
 }
 export default Cards
---
+++ abode/src/components/Card/cardData.js
@@ -0,0 +1,42 @@
+import hotel from "../../assets/img/hotel1.jpg"
+
+export default function CardData(){
+    const cardData=[
+        {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    },
+    {
+        title:"Place1",
+        rating:4.88,
+        image:hotel        
+    }
+    ]
+    return cardData;
+}
